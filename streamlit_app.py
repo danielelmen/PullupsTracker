e502a4f155c86bfa0c97ef9046000f24904cd3ae
danielelmen <daniel-ec@hotmail.com>
2025-10-21 20:55:47 +0200

    Update Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ st.write(f"Du er logget ind som: {user}")
 ################ Google sheets test
 
 SHEET_TITLE = "PullupsSheet"   # your Google Sheet name
-TAB_NAME    = "daniel"            # the tab name
+TAB_NAME    = "Sheets1"            # the tab name
 
 @st.cache_resource
 def get_ws():
